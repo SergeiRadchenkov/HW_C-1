586f4f5c5d799d4c9c7d14cbbfd48b83bc4a6368
Sergei <radchenkovsa@yandex.ru>
2023-08-15 12:52:49 +0300

    Выводим число из трёхзначного числа

--- case4/Program.cs
+++ case4/Program.cs
@@ -1,12 +1,9 @@
 ﻿Console.Write("Введите число: ");
 int number = int.Parse(Console.ReadLine()!);
 int start = -number;
-while(start <= number)
+while(start < number)
 {
-    Console.Write(start);
-    if(start != number)
-    {
-        Console.Write(", ");
-    }
+    Console.Write(start + ", ");
     start++;
 }
+Console.WriteLine(number);
---
+++ case5/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите трёхзначное число: ");
+int number = int.Parse(Console.ReadLine()!);
+int thirdDigit = number %10;
+int secondDigit = number /10 %10;
+int firstDigit = number /100;
