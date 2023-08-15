84f5d655ee36eecb1e55764531ab0af00d3b0969
Sergei <radchenkovsa@yandex.ru>
2023-08-15 10:12:30 +0300

    определение числа дроби двух чисел

--- case2/Program.cs
+++ case2/Program.cs
@@ -1,2 +1,12 @@
-﻿ConsoleWrite("Введите число 1");
-ConsoleWrite("Введите число 2");
+﻿Console.Write("Введите число 1: ");
+int number1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число 2: ");
+int number2 = int.Parse(Console.ReadLine()!);
+if(number2 * number2 == number1)
+{
+    Console.WriteLine($"Число {number1} является квадратом числа {number2}");
+}
+else
+{
+    Console.WriteLine($"Число {number1} не является квадратом числа {number2}");
+}
