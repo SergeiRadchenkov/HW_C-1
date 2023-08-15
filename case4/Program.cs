6f67e89925e8501ca3105a3321a35b33ab3f3428
Sergei <radchenkovsa@yandex.ru>
2023-08-15 11:50:31 +0300

    Нахождение целых чисел

--- case4/Program.cs
+++ case4/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+int start = -number;
+while(start <= number)
+{
+    Console.Write(start);
+    if(start != number)
+    {
+        Console.Write(", ");
+    }
+    start++;
+}
