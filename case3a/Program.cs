9b2fe5c539a6eb31ebd58e16642a70ae4dd70928
Sergei <radchenkovsa@yandex.ru>
2023-08-15 11:03:00 +0300

    Определение дня недели Вариант2

--- case3a/Program.cs
+++ case3a/Program.cs
@@ -1,1 +1,4 @@
-﻿
+﻿Console.Write("Введите дня недели от 1 до 7: ");
+int weekday = int.Parse(Console.ReadLine()!);
+string[] days = {"Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"};
+Console.WriteLine(days[weekday - 1]);
