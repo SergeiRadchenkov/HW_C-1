fa29971cf8953bfc4d274723f45585cf4cd90d82
Sergei <radchenkovsa@yandex.ru>
2023-08-15 09:29:07 +0300

    возведение числа в дробь

--- case1/Program.cs
+++ case1/Program.cs
@@ -1,4 +1,7 @@
 ﻿Console.Write("Введите число: "); // "456"
-string num = Console.ReadLine(); // "456"
-int number = int.Parse(num);
+// string num = Console.ReadLine()!; // "456"
+// int number = int.Parse(num);
+int number = int.Parse(Console.ReadLine()!);
 int square = number * number;
+Console.WriteLine("Квадрат числа " + number + " равен " + square);
+Console.WriteLine($"Квадрат числа {number} равен {square}");
