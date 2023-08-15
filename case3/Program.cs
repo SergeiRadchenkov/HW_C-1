a12c5c1aed73381ced48fa6cac95c2736441f7d5
Sergei <radchenkovsa@yandex.ru>
2023-08-15 10:31:11 +0300

    Определение дня недели

--- case3/Program.cs
+++ case3/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите дня недели от 1 до 7: ");
+int weekday = int.Parse(Consolr.WriteLine()!);
+
+if(weekday == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+else if(weekday == 2)
+{
+    Console.WriteLine("Вторник");
+}
+else if(weekday == 3)
+{
+    Console.WriteLine("Среда");
+}
+else if(weekday == 4)
+{
+    Console.WriteLine("Четверг");
+}
+else if(weekday == 5)
+{
+    Console.WriteLine("Пятница");
+}
+else if(weekday == 6)
+{
+    Console.WriteLine("Суббота");
+}
+else if(weekday == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
+else
+{
+    Console.WriteLine("Такого дня недели нет");
+}
