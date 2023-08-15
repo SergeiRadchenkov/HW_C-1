e17b217eec4786c02a6a8891c9bbf3c39c1daa5f
Sergei <radchenkovsa@yandex.ru>
2023-08-15 13:02:06 +0300

    Выводим число из трёхзначного числа

--- case5/Program.cs
+++ case5/Program.cs
@@ -3,3 +3,6 @@ int number = int.Parse(Console.ReadLine()!);
 int thirdDigit = number %10;
 int secondDigit = number /10 %10;
 int firstDigit = number /100;
+Console.WriteLine($"Первая цифра {number} равна {firstDigit}");
+Console.WriteLine($"Вторая цифра {number} равна {secondDigit}");
+Console.WriteLine($"Третья цифра {number} равна {thirdDigit}");
